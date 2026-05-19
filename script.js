9b1d2fa1c124a4874d44558c8b138cf8a18f92bd
kamlesh04yadav15-ux <kamlesh04yadav15@gmail.com>
2026-05-19 11:43:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculateTax(){
+
+  let profit =
+  Number(
+    document.getElementById("profit").value
+  );
+
+  let tax = 0;
+
+  if(profit > 375000){
+
+    tax =
+    (profit - 375000) * 0.09;
+  }
+
+  document.getElementById("result")
+  .innerHTML =
+  "Corporate Tax: AED " +
+  tax.toFixed(2);
+}
